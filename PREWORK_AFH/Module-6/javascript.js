d989524d4fa9a5ce53feee915d1998725502df64
matthewmaldonado27 <87158759+matthewmaldonado27@users.noreply.github.com>
2021-07-18 20:52:13 -0500

    Add files via upload

---
+++ PREWORK_AFH/Module-6/javascript.js
@@ -0,0 +1,10 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "200";
+    
+    });
+    document.getElementById("button2").addEventListener("click", function(){
+
+        document.getElementById("box").style.color = "blue";
+        
+        });
